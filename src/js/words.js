ea1d3a1716530346c430c60c3abed6367a0f683c
Marco Rieger <marco.rieger@aubi-plus.de>
2015-02-05 13:41:19 +0100

    initial commit

---
+++ src/js/words.js
@@ -0,0 +1,16 @@
+var words = [
+    'Domain Controller',
+    'Switch',
+    'Netzwerk',
+    'Access',
+    'Routing',
+    'Sicherheit',
+    'HARTING',
+    'Layer',
+    'Lernaufgaben',
+    'Physikalisch',
+    'Rahden',
+    'Server',
+    'Switch',
+    'Redundanz',
+];
